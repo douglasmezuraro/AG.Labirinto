f44e534d69355311ee6c4752c43f75ab5fe49708
Douglas Mezuraro <douglasmez@gmail.com>
2017-07-05 13:50:36 -0300

    Criado projeto.

---
+++ Labirinto/src/Entidades/Cor.java
@@ -0,0 +1,7 @@
+package Entidades;
+
+public enum Cor {
+    Branco,
+    Cinza, 
+    Preto;
+}
---
+++ Labirinto/src/labirinto/Labirinto.java
@@ -0,0 +1,9 @@
+package labirinto;
+
+public class Labirinto {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
